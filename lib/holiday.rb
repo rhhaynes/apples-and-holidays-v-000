848acbacff6f329ed2fe031f56b6f65e6da23fe7
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-26 16:32:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,6 +36,6 @@ end
 def all_holidays_with_bbq(holiday_hash)
   arr = []
   holiday_hash.each do |sea,sea_hash|
-    arr << sea if sea_hash.values.flatten.any?{}
+    arr << sea if sea_hash.values.flatten.any?{|v| v=="BBQ"}
   end
 end
