0a0b34497690b72a6f8b6ae23a277b07ab1a15be
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-26 16:15:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,21 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 # ---------------------------------------------
-
+holiday_supplies = {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+}
 all_supplies_in_holidays(holiday_supplies)
 # ---------------------------------------------
 
