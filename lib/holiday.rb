c77b2c3f5de07c0b9eb0c81278bdcf283947706e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-26 16:12:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,season_hash|
     puts "#{season.to_s.capitalize}:"
     season_hash.each do |holiday,arr|
-      print "  #{holiday.to_s.tr("_"," ").split.map(&:capitalize).join(" ")}: "
+      print " #{holiday.to_s.tr("_"," ").split.map(&:capitalize).join(" ")}: "
       puts "#{arr.join(", ")}"
     end
   end
